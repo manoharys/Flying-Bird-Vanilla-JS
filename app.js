27e7f6dcf1521d3b10c164bb831ddc32af8f39c1
manohar <manumanohar1527@gmail.com>
2020-05-02 22:49:07 +0530

    setting up Game structures

---
+++ app.js
@@ -0,0 +1,6 @@
+const score = document.querySelector('.score');
+const gameMessage = document.querySelector('.gameMessage');
+const gameStart = document.querySelector('.startScreen');
+const gameArea = document.querySelector('.gameArea');
+
+
