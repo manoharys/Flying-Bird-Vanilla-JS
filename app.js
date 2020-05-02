9ebef893915ae9ad0d84f698728f283f210163f7
manohar <manumanohar1527@gmail.com>
2020-05-02 22:55:47 +0530

    tracking 'keyBoard Events'

--- app.js
+++ app.js
@@ -3,4 +3,28 @@ const gameMessage = document.querySelector('.gameMessage');
 const gameStart = document.querySelector('.startScreen');
 const gameArea = document.querySelector('.gameArea');
 
+//Game trigger when user clicks this
+gameMessage.addEventListener('click', start);
+gameStart.addEventListener('click', start);
 
+
+//Setting event listeners to track keyBoard events
+document.addEventListener('keydown', pressOn);
+document.addEventListener('keyup', pressOff);
+
+//KeyBoard events
+function pressOn(e) {
+    console.log(e);
+    console.log(e.code);
+}
+
+function pressOff(e) {
+    console.log(e)
+    console.log(e.code);
+}
+
+
+//Function which starts the gamePlay..
+function start() {
+    console.log("Hyy there game started");
+}
