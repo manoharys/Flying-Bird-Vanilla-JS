510a4a57638e672619576f5f0c3f82151accf873
manohar <manumanohar1527@gmail.com>
2020-05-03 21:07:26 +0530

    created "obstacles"

--- app.js
+++ app.js
@@ -55,8 +55,46 @@ function start() {
     gameArea.appendChild(bird);
     player.x = bird.offsetLeft;
     player.y = bird.offsetTop;
+    player.pipe = 0;
+    let spacing = 300;
+    let howMany = Math.floor((gameArea.offsetWidth) / spacing);
+    console.log(howMany);
+    for (let x = 0; x < howMany; x++) {
+        buildPipes(player.pipe * spacing);
+    }
     window.requestAnimationFrame(playGame);
 }
+
+function buildPipes(startPos) {
+    let totalHeight = gameArea.offsetHeight;
+    let totalWidth = gameArea.offsetWidth;
+    player.pipe++;
+    let pipe1 = document.createElement("div");
+    pipe1.start = startPos + totalWidth;
+    pipe1.classList.add("pipe");
+    pipe1.innerHTML = "<br>" + player.pipe;
+    pipe1.height = Math.floor(Math.random() * 350);
+    pipe1.style.height = pipe1.height + "px";
+    pipe1.style.left = pipe1.start + "px";
+    pipe1.style.top = "0px";
+    pipe1.x = pipe1.start;
+    pipe1.id = player.pipe;
+  
+    gameArea.appendChild(pipe1);
+    let pipeSpace = Math.floor(Math.random() * 250) + 150;
+    let pipe2 = document.createElement("div");
+    pipe2.start = pipe1.start;
+    pipe2.classList.add("pipe");
+    pipe2.innerHTML = "<br>" + player.pipe;
+    pipe2.style.height = totalHeight - pipe1.height - pipeSpace + "px";
+    pipe2.style.left = pipe1.start + "px";
+    pipe2.style.bottom = "0px";
+    pipe2.x = pipe1.start;
+    pipe2.id = player.pipe;
+
+    gameArea.appendChild(pipe2);
+}
+
 
 
 function playGame() {
@@ -107,5 +145,5 @@ function endGame(bird) {
     bird.setAttribute('style', "transform:rotate(180deg");
     score.classList.add('hide');
     gameMessage.insertAdjacentHTML('beforeend', `<p style="color:red;letter-spacing:3px;font-family:fantasy;margin-bottom:10px;">GAME OVER!!!</p><br>YOUR SCORE = ${player.score}<br><br>play again`);
-   
+
 }
